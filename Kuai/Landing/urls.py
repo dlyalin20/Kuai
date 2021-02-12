754c25dfef5d696b9ee3b7cfda9166b3cfd929a6
jasonjiangstuy <jasonjiangny@gmail.com>
2021-02-11 23:44:18 -0500

    sync changes, error with search

--- Kuai/Landing/urls.py
+++ Kuai/Landing/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
-
+app_name = 'Landing'
 urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login_view, name='login'),
-    path('register', views.register_view, name='register')
+    path('register', views.register_view, name='register'),
+    # tentitive location may put in diffrent project
+    path('search', views.search, name='search'),
+    path('autocomplete', views.autocomplete_view, name="autocomplete"),
+    path('<int:location_id>/go', views.go, name="go"),
 ]

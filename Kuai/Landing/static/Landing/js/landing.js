73ab1a9caea43e119235df1a1ea29b69ac7faff8
jasonjiangstuy <jasonjiangny@gmail.com>
2021-03-04 16:07:57 -0500

    working search => place id => map

--- Kuai/Landing/static/Landing/js/landing.js
+++ Kuai/Landing/static/Landing/js/landing.js
@@ -4,7 +4,10 @@ var autocomplete;
 function initialize() {
     autocomplete = new google.maps.places.Autocomplete(
         /** @type {HTMLInputElement} */(autocomplete_input),
-        { types: ['geocode'], fields: ['place_id', 'name', 'types'] });
+        { 
+        // types that we will auto compelete for + search for
+        types: ['geocode', 'establishment'],
+        fields: ['place_id'] });
     google.maps.event.addListener(autocomplete, 'place_changed', search);
     $('#search-submit').on('click', function(event){
         event.preventDefault();
@@ -14,36 +17,16 @@ function initialize() {
     function search() {
         const place = autocomplete.getPlace();
         console.log(place);
-        console.log(JSON.stringify(place));
-        var fd = new FormData();
-        var target;
         if (place == undefined) {
             // User entered the name of a Place that was not suggested and
             // pressed the Enter key, or the Place Details request failed.
-            // window.alert("No details available for input: '" + place.name + "'");
-            // fd.append('q', autocomplete_input.value );
-            // target = "search"
+            //=> route to advanced search
             window.location.href = '/search?q=' + autocomplete_input.value;
           }
         //   good this exists, go to go screen with this data
         else{
             console.log(place.place_id);
-            var fd = new FormData();
-            fd.append('id', place.place_id);
-            target = "go"
+            window.location.href = '/go?q='+ autocomplete_input.value + '&id=' + place.place_id;
         }
-        
-        $.ajax({ 
-            type: "GET",
-            url : target,
-            // url:'', 
-            data : fd,
-            async : false, // we are leaving this page
-            processData: false,
-            contentType: "multipart/form-data",
-            success : function(result){
-                console.log(result);
-            }
-        });
     }
 }

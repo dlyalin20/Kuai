19bac1d86fac5c6f3a36009918f4df896bcfa59f
jasonjiangstuy <jasonjiangny@gmail.com>
2021-07-02 22:34:06 -0400

    planned out heat map => switch to improve code for the generic Business class

---
+++ Kuai/Landing/static/Landing/js/heatmap.js
@@ -0,0 +1,7 @@
+/**
+ * renders a heat map of the user's visable surroundings 
+ * @param {Business[]} bisArray
+ */
+function render(bisArray){
+    // for each wait biz, collect the wait time and plot on heat map
+}

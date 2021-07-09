a2b2dc62697ca3ffec5e4f5d46a4b3e288ab823d
jasonjiangstuy <jasonjiangny@gmail.com>
2021-07-09 13:42:04 -0400

    added testing code with better icons and location marker

---
+++ testheatmap/index.js
@@ -0,0 +1,97 @@
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+
+var heatmap;
+initAutocomplete()
+function initAutocomplete() {
+    var heatmapData = [
+      new google.maps.LatLng(37.782, -122.447),
+      new google.maps.LatLng(37.782, -122.445),
+      new google.maps.LatLng(37.782, -122.443),
+      new google.maps.LatLng(37.782, -122.441),
+      new google.maps.LatLng(37.782, -122.439),
+      new google.maps.LatLng(37.782, -122.437),
+      new google.maps.LatLng(37.782, -122.435),
+      new google.maps.LatLng(37.785, -122.447),
+      new google.maps.LatLng(37.785, -122.445),
+      new google.maps.LatLng(37.785, -122.443),
+      new google.maps.LatLng(37.785, -122.441),
+      new google.maps.LatLng(37.785, -122.439),
+      new google.maps.LatLng(37.785, -122.437),
+      new google.maps.LatLng(37.785, -122.435)
+      // 14
+    ];
+    
+    var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: sanFrancisco,
+      zoom: 13,
+      mapTypeId: "roadmap",
+    });
+
+    var GeoMarker = new GeolocationMarker(map);
+    heatmap = new google.maps.visualization.HeatmapLayer({
+      data: heatmapData
+    });
+    heatmap.setMap(map);
+    // Create the search box and link it to the UI element.
+    const input = document.getElementById("pac-input");
+    const searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener("bounds_changed", () => {
+      searchBox.setBounds(map.getBounds());
+    });
+    let markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener("places_changed", () => {
+      const places = searchBox.getPlaces();
+  
+      if (places.length == 0) {
+        return;
+      }
+      // Clear out the old markers.
+      markers.forEach((marker) => {
+        marker.setMap(null);
+      });
+      markers = [];
+      // For each place, get the icon, name and location.
+      const bounds = new google.maps.LatLngBounds();
+      places.forEach((place) => {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        const icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25),
+        };
+        // Create a marker for each place.
+        markers.push(
+          new google.maps.Marker({
+            map,
+            icon,
+            title: place.name,
+            position: place.geometry.location,
+          })
+        );
+  
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+  }
